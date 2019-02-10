cb1b901663e881aa074ef7e267b69c5451fb1fdd
aawadall <aawadall@ualberta.ca>
2019-02-10 14:58:09 -0500

    refactor date formatter

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const moment = require('moment');
 
+function dateFormatter() {
+    return moment().format('Do MMMM YYYY, hh:mm:ss');
+}
+
 exports.nodeHTTP = function entryHTTP(req, resp) {
-    resp.status(200).send(moment().format('Do MMMM YYYY, hh:mm:ss'));
+    resp.status(200).send(dateFormatter());
 };
 
 exports.nodePubsub = function entryPubsub(event, callback) {
-    console.log(`Hello PUBSUB! ${moment().format('Do MMMM YYYY, hh:mm:ss')}`);
+    console.log(`Hello PUBSUB! ${dateFormatter()}`);
     callback();
 };
 
 exports.nodeStorage = function entryStorage(event, callback) {
-    console.log(`Hello STORAGE! ${moment().format('Do MMMM YYYY, hh:mm:ss')}`);
+    console.log(`Hello STORAGE! ${dateFormatter()}`);
     callback();
 };
