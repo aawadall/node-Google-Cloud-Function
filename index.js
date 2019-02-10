dba6f8043b8221a2aef45401bcf9b8db85afa021
aawadall <aawadall@ualberta.ca>
2019-02-10 15:05:34 -0500

    skeleton ready

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const dateFormatter = require('dateFormatter');
+const formatter = require('./dateFormatter');
 
 
 exports.nodeHTTP = function entryHTTP(req, resp) {
-    resp.status(200).send(dateFormatter());
+    resp.status(200).send(formatter.dateFormatter());
 };
 
 exports.nodePubsub = function entryPubsub(event, callback) {
-    console.log(`Hello PUBSUB! ${dateFormatter()}`);
+    console.log(`Hello PUBSUB! ${formatter.dateFormatter()}`);
     callback();
 };
 
 exports.nodeStorage = function entryStorage(event, callback) {
-    console.log(`Hello STORAGE! ${dateFormatter()}`);
+    console.log(`Hello STORAGE! ${formatter.dateFormatter()}`);
     callback();
 };
