f6079586662fc6ebf6fec143cb83d0d932ff694b
aawadall <aawadall@ualberta.ca>
2019-02-13 17:21:32 -0500

    scaffold for storage function

--- data-functions/index.js
+++ data-functions/index.js
@@ -1,3 +1,20 @@
 const Datastore = require('@google-cloud/datastore');
 const Storage = require('@google-cloud/storage');
 const Vision = require('@google-cloud/vision');
+
+const datastore = Datastore();
+const storage = Storage();
+const client = new Vision.ImageAnnotatorClient();
+
+exports.imageTagger = (event, callback) => {
+    const object = event.data;
+    console.log(object);
+
+    if (!object.contentType.startsWith('image/')) {
+        console.log('This is not an image');
+        callback();
+        return;
+    }
+
+
+}
