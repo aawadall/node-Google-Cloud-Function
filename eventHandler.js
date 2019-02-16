bff1d71da7729c6e38a3b45f22c9bf0aced7f80b
aawadall <aawadall@ualberta.ca>
2019-02-15 21:35:25 -0500

    Event handler module

---
+++ eventHandler.js
@@ -0,0 +1,10 @@
+
+// Validate Event Contents
+exports.validateEvent = (event) => {
+    /* TODO: event validation mechanism
+        Build a set of methods to validate event object passed based on predefined rules
+     */
+    // 1. check event has minimum required fields
+    // 2. check event types has its special fields
+};
+// Save Event in file
